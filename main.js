949ae81f1b6d4e31d13562bdfa97596f280cd3ed
Andrea <andreademarco91@gmail.com>
2020-02-02 16:57:28 +0100

    BASE OK

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-	//Code
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/array/music",
+      method: "GET",
+      success: function (data, stato)
+      {
+        console.log(data.response);
+        var dischi = data.response;
+        console.log(dischi);
+        for (var i = 0; i < dischi.length; i++) {
+          var disco = dischi[i];
+          console.log(disco);
+          var source = $('.handlebar').html();
+          console.log(source);
+
+          var template = Handlebars.compile(source);
+
+          var context = disco;
+          var html = template(context);
+
+          $('.cds-container').append(html);
+
+
+
+        }
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+    }
+  );
+
+
 });
