46b7c7c0a86c7b57dedffc5a4c9b8bc6724ebbd9
Andrea <andreademarco91@gmail.com>
2020-02-02 17:11:43 +0100

    START SELECT OPTION

--- main.js
+++ main.js
@@ -20,9 +20,6 @@ $(document).ready(function() {
           var html = template(context);
 
           $('.cds-container').append(html);
-
-
-
         }
       },
       error: function (richiesta, stato, errori) {
@@ -30,6 +27,37 @@ $(document).ready(function() {
       }
     }
   );
+});
 
+$('select').change(function(){
+  var genere = $(this).val();
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/array/music",
+      method: "GET",
+      success: function (data, stato)
+      {
+        console.log(data.response);
+        var dischi = data.response;
+        console.log(dischi);
+        for (var i = 0; i < dischi.length; i++) {
+          var disco = dischi[i];
+          console.log(disco);
+          var source = $('.handlebar').html();
+          console.log(source);
+
+          var template = Handlebars.compile(source);
+
+          var context = disco;
+          var html = template(context);
+
+          $('.cds-container').append(html);
+        }
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+    }
+  );
 
 });
